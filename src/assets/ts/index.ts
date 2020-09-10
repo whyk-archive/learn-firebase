d0eed52b4632375bb287dfd4056800bd7ad7454a
WhyK <windchimeyk@gmail.com>
2020-09-10 15:48:24 +0900

    update: UI機能を単一のエクスポートに統合

--- src/assets/ts/index.ts
+++ src/assets/ts/index.ts
@@ -1,6 +1,4 @@
-import { prism } from './modules/ui/syntax-highlight'
-import { indexAutoGen } from './modules/ui/index-autogen'
-import { smoothScroll } from './modules/ui/smooth-scroll'
+import { prism, indexAutoGen, smoothScroll } from './modules/ui/index'
 
 prism()
 indexAutoGen()
