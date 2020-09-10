0fd1217db10d5a742a2219c63bce0f07d4854d3f
WhyK <windchimeyk@gmail.com>
2020-09-10 23:14:06 +0900

    update: 現状のコードを追加

--- src/assets/ts/index.ts
+++ src/assets/ts/index.ts
@@ -1,5 +1,64 @@
 import { prism, indexAutoGen, smoothScroll } from './modules/ui/index'
+import { initializeApp, analytics, database, firestore } from 'firebase'
+import type { firebaseConfig } from './interface/firebase'
+import { config } from 'dotenv'
 
 prism()
 indexAutoGen()
 smoothScroll()
+
+config()
+
+const fbconfig: firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASUREMENT_ID,
+}
+initializeApp(fbconfig)
+analytics()
+
+/**
+ * Firestore
+ */
+const store = firestore()
+const $sendToFirestore: HTMLButtonElement = document.querySelector(
+  '#SendToFirestore'
+)
+$sendToFirestore.addEventListener('click', async () => {
+  const $username: HTMLInputElement = document.querySelector('#FirestoreName')
+  const $description: HTMLInputElement = document.querySelector(
+    '#FirebaseDescription'
+  )
+  try {
+    const docRef = await store.collection('user').add({
+      username: $username.value,
+      description: $description.value,
+    })
+    console.log(`Document add: ${docRef}`)
+  } catch (error) {
+    console.error(`Document adding error: ${error}`)
+  }
+})
+
+/**
+ * Realtime Database
+ */
+const db = database()
+const $sendToDatabase: HTMLButtonElement = document.querySelector(
+  '#SendToDatabase'
+)
+$sendToDatabase.addEventListener('click', () => {
+  const $username: HTMLInputElement = document.querySelector('#DatabaseName')
+  const $description: HTMLInputElement = document.querySelector(
+    'DatabaseDescription'
+  )
+  db.ref('users').set({
+    username: $username.value,
+    description: $description.value,
+  })
+})
---
+++ src/assets/ts/interface/firebase.ts
@@ -0,0 +1,10 @@
+export interface firebaseConfig {
+  apiKey: string
+  authDomain: string
+  databaseURL: string
+  projectId: string
+  storageBucket: string
+  messagingSenderId: string
+  appId: string
+  measurementId: string
+}
